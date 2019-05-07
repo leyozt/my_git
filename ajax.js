6f14ff16f7aac538975ba49faf50c3e1d03db236
leyozt <282371699@qq.com>
2019-05-07 23:05:04 +0800

    add a ajax-js POST.

--- ajax.js
+++ ajax.js
@@ -12,4 +12,18 @@
        
        xhr.send(null);
     }
+
+    //原生javascript-->ajax "POST"
+    function AjaxSubmit(){
+       var xhr = new XMLHttpRequest();
+       xhr.onreadstatechange = function(){
+           if(xhr.readyState == 4){
+		//接收完毕服务器返回的数据
+	      console.log(xhr.responseText);
+           }
+       };
+       xhr.open('POST','/url.html');
+       xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded;charset-UTF-8');
+       xhr.send("p=456");
+    }
 <script>
